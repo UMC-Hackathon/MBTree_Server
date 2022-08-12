bd36815fc8e0cb6e9f5c7b6e1d2a7aacab4a635e
dahyuun <mdahyun1025@naver.com>
2022-08-12 20:19:39 +0900

    access token + 사용자 정보 받아오기

--- src/main/java/com/mbtree/mbtree/post/OAuthController.java
+++ src/main/java/com/mbtree/mbtree/post/OAuthController.java
@@ -1,8 +1,11 @@
 package com.mbtree.mbtree.post;
 
+import com.fasterxml.jackson.databind.DatabindContext;
 import com.mbtree.mbtree.service.OAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @AllArgsConstructor
@@ -19,5 +22,14 @@ public class OAuthController {
         System.out.println("code : " + code);
         String access_Token = OAuthService.getKakaoAccessToken(code);
         System.out.println("controller access_token : " + access_Token);
+
+        HashMap<String, Object> userInfo =  OAuthService.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+//        if (userInfo.get("email") != null) {
+//            session.setAttribute("userId", userInfo.get("email"));
+//            session.setAttribute("access_Token", access_Token);
+//        }
     }
 }
