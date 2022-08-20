4aef7e065890b485286cb8dfd239f689d977fb9d
ji-vvon <jjw9895@duksung.ac.kr>
2022-08-20 19:10:54 +0900

    ãweb url 설정

---
+++ src/main/java/com/mbtree/mbtree/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.mbtree.mbtree.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class WebController {
+
+    @RequestMapping("/main")
+    public String mainPage(){
+        return "main.html";
+    }
+
+}
